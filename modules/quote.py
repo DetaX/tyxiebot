0783636ff34a3b52183a86637404e25143b3f064
Pierre Caretero <pierre@detax.eu>
2015-03-14 19:46:09 +0100

    adding quote search command

--- modules/quote.py
+++ modules/quote.py
@@ -8,12 +8,13 @@ quote_table_name = 'quote'
 
 
 class Quote:
-    def __init__(self, citation, date_time):
-        self.citation = citation
+    def __init__(self, id, message, date_time):
+        self.id = id
+        self.message = message
         self.datetime = date_time
 
     def __repr__(self):
-        return self.citation
+        return self.message
 
 
 class QuoteManager:
@@ -21,19 +22,25 @@ class QuoteManager:
         self.conn = sqlite3.connect(quote_db)
         self.cur = self.conn.cursor()
         self.cur.execute(
-            'create table if not exists ' + quote_table_name + ' (id integer primary key autoincrement, citation text, datetime text)')
+            'create table if not exists ' + quote_table_name + ' (id integer primary key autoincrement, message text, datetime text)')
         self.conn.commit()
 
     def __del__(self):
         self.cur.close()
         self.conn.close()
 
-    def add_quote(self, quote):
-        self.cur.execute('insert into ' + quote_table_name + ' (citation, datetime) values (?,?)',
-                         (quote.citation, str(quote.datetime)))
-        self.conn.commit()
+    def get_next_id(self):
         self.cur.execute('select id from ' + quote_table_name + ' order by id desc limit 1')
-        return self.cur.fetchone()[0]
+        id = self.cur.fetchone()
+        if id:
+            return id[0] + 1
+        else:
+            return 1
+
+    def add_quote(self, quote):
+        self.cur.execute('insert into ' + quote_table_name + ' (message, datetime) values (?,?)',
+                         (quote.message, str(quote.datetime)))
+        self.conn.commit()
 
     def del_quote(self, quote_id):
         self.cur.execute('delete from ' + quote_table_name + ' where id=?', (quote_id,))
@@ -43,12 +50,16 @@ class QuoteManager:
         self.cur.execute('select * from ' + quote_table_name + ' where id=?', (quote_id,))
         fetch = self.cur.fetchone()
         if fetch:
-            return Quote(citation=fetch[1], date_time=fetch[2])
+            return Quote(id=fetch[0], message=fetch[1], date_time=fetch[2])
 
     def get_quotes(self):
+        quotes = self.search_quote('')
+        return quotes
+
+    def search_quote(self, message):
         quotes = []
-        for row in self.cur.execute('select * from ' + quote_table_name):
-            quote = Quote(citation=row[1], date_time=row[2])
+        for row in self.cur.execute('select * from ' + quote_table_name + ' where message like \'%' + message + '%\''):
+            quote = Quote(id=row[0], message=row[1], date_time=row[2])
             quotes.append(quote)
         return quotes
 
@@ -63,18 +74,23 @@ def on_pubmsg(self, serv, ev):
     if msg_split[0] == '!quote':
         if msg_split[1] == 'add':
             if len(msg_split) > 3:
-                quote = Quote(citation=' '.join(msg_split[2:]), date_time=datetime.now())
-                quote_id = qm.add_quote(quote)
-                serv.privmsg(chan, 'La citation a été ajouté ! (' + str(quote_id) + ')')
+                quote = Quote(id=qm.get_next_id(), message=' '.join(msg_split[2:]), date_time=datetime.now())
+                qm.add_quote(quote)
+                serv.privmsg(chan, 'La citation a été ajouté ! (' + str(quote.id) + ')')
         if msg_split[1] == 'show':
             if len(msg_split) > 2:
                 quote = qm.get_quote(msg_split[2])
                 if quote:
-                    serv.privmsg(chan, '(' + quote.datetime + ') ' + quote.citation)
+                    serv.privmsg(chan, '(' + quote.datetime + ') ' + quote.message)
                 else:
                     serv.privmsg(chan, 'La citation ' + msg_split[2] + ' n\'existe pas')
         if msg_split[1] == 'list':
             quotes = qm.get_quotes()
             for quote in quotes:
-                serv.privmsg(chan, '(' + quote.datetime + ') ' + quote.citation)
+                serv.privmsg(chan, '#' + str(quote.id) + ' : (' + quote.datetime + ') ' + quote.message)
+        if msg_split[1] == 'search':
+            if len(msg_split) > 2:
+                quotes = qm.search_quote(' '.join(msg_split[2:]))
+                for quote in quotes:
+                    serv.privmsg(chan, '#' + str(quote.id) + ' : (' + quote.datetime + ') ' + quote.message)
 
